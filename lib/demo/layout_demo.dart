9b6cfb0f18277c9b5aa1aee0810c9ed0569258d5
Vantch <ui@vantch.com>
2019-08-26 10:58:19 +0800

    SizeBox 固定尺寸的盒子

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -3,7 +3,53 @@ import 'package:flutter/material.dart';
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return IconRow();
+    return SizeBox();
+  }
+}
+
+class SizeBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: 200.0,
+            height: 300.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Icon(
+                Icons.ac_unit,
+                size: 64.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 32.0,
+          ),
+          SizedBox(
+            width: 160.0,
+            height: 60.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Icon(
+                Icons.brightness_2,
+                color: Colors.white,
+                size: 32.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -16,7 +62,10 @@ class IconRow extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           IconBadge(Icons.pool),
-          IconBadge(Icons.beach_access, size: 68.0,),
+          IconBadge(
+            Icons.beach_access,
+            size: 68.0,
+          ),
           IconBadge(Icons.airplanemode_active),
         ],
       ),
@@ -33,7 +82,11 @@ class IconBadge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Icon(icon, size: size, color: Colors.white,),
+      child: Icon(
+        icon,
+        size: size,
+        color: Colors.white,
+      ),
       width: size + 60.0,
       height: size + 60.0,
       color: Color.fromRGBO(3, 54, 255, 1.0),
